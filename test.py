82be6203d908f889d805eb9731fe0bd15efe5c94
ajayxaviera <147126399+ajayxaviera@users.noreply.github.com>
2024-07-30 15:25:28 +0530

    Update test.py

--- test.py
+++ test.py
@@ -106,11 +106,10 @@ def get_job_details(pipeline, stage=None, status=None):
     job_names = []
     check_stage_status = True
     if pipeline.lower() == "all":
-         with open('pipelines.yaml', 'r') as file:
+        with open('pipelines.yaml', 'r') as file:
             data = yaml.safe_load(file)
 
         job_names = data['pipelines']
-        # job_names = ["MyPipeline1", "MyPipeline2", "MyPipeline3", "MyPipeline4"]
         check_stage_status = False
     else:
         job_names.append(pipeline)
